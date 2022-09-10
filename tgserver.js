ea5a7600095328ee494b0c6a09c3fb703cf22852
xakerbox <kuzinvt@gmail.com>
2022-09-10 13:35:57 +0300

    Attached dream calc

--- tgserver.js
+++ tgserver.js
@@ -6,28 +6,56 @@ const token = "5405704788:AAFFoHQJj_st8Lyo3ufi6Eo-bBulirLN3sA";
 const chatIds = [165564370, 535043367]; // 535043367
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", async (msg) => {
+let constant = 0;
+
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  if (msg.text === "🪙 COINS 🪙" || msg.text === "All") {
-    bot.sendMessage(chatId, await getAllOpened());
-  }
 
-  if (msg.text === "🏦 BALANCE 🏦") {
-    bot.sendMessage(chatId, await getWalletBalance());
-  }
-});
-
-bot.onText(/\/start/, (msg, ) => {
-  bot.sendMessage(msg.chat.id, "Чё надо?", {
+  bot.sendMessage(chatId, "Чё надо?", {
     reply_markup: {
       keyboard: [["🏦 BALANCE 🏦"], ["🪙 COINS 🪙"], ["CALC"]],
     },
   });
-});
 
-// bot.onText("CALC", (msg, match) => {
-//   bot.sendMessage(
-//     msg.chat.id,
-//     "Введи данные через запятую в формате: \n СТАРТ_ДЕПО, ЖЕЛАЕМЫЙ_ДЕПО, СУТОЧНЫЙ ПРОЦЕНТ\n\nПример:\n1000, 1000, 0.5"
-//   );
-// });
+
+  bot.on("message", async (msg) => {
+
+    if (msg.text === "🪙 COINS 🪙") {
+      bot.sendMessage(chatId, await getAllOpened());
+    }
+
+    if (msg.text === "🏦 BALANCE 🏦") {
+      bot.sendMessage(chatId, await getWalletBalance());
+    }
+
+    if (msg.text === "CALC") {
+      await bot.sendMessage(
+        chatId,
+        "Введи данные через запятую в формате: \n СТАРТ_ДЕПО, ЖЕЛАЕМЫЙ_ДЕПО, СУТОЧНЫЙ ПРОЦЕНТ\n\nПример:\n1000, 1000, 0.5\n", {
+        reply_markup: {
+          keyboard: [["ВЫЙТИ"]],
+        }
+      })
+
+      constant = 1;
+      return
+    }
+
+    if (msg.text === 'ВЫЙТИ') {
+      bot.sendMessage(chatId, 'Выходим...', {
+        reply_markup: {
+          keyboard: [["🏦 BALANCE 🏦"], ["🪙 COINS 🪙"], ["CALC"]],
+        }
+      })
+    }
+
+
+    if (constant === 1) {
+      const data = msg.text.trim().split(',');
+      bot.sendMessage(chatId, `Миллионером будешь через ${dreamCalc(+data[0], +data[1], +data[2])} дней(я)!`);
+      constant = 0;
+      return
+    }
+  });
+
+});
