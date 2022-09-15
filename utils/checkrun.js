1fff9e621028bb3113689d866e0d8fb8088f9d15
xakerbox <kuzinvt@gmail.com>
2022-09-15 22:58:57 +0300

    Check bot status

--- utils/checkrun.js
+++ utils/checkrun.js
@@ -4,13 +4,14 @@ const checkRunBot = async (coinName) => {
   let result;
   exec(`ps aux | grep ${coinName}_bot | grep node`, (error, stdout, stderr) => {
     if (stdout) {
+      console.log(result);
+
       result = true;
       return;
     }
     return false;
   });
 
-  console.log(result);
 };
 
 module.exports = { checkRunBot };
